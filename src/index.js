2114870a26ddb435c1c3d9aeed15fa7d7eca11fe
ZeldOcarina <mattia.r86@gmail.com>
2022-06-09 16:52:38 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+import * as bootstrap from "bootstrap";
+import './js/navbar'
+
 if (module.hot) {
     module.hot.accept();
 }
 
 console.log('%c Welcome to Parcel HTML Starter!!', 'background: #bdc7ff; color: darkred; font-size: 24px; font-weight: bold; padding: 10px;');
+//document.getElementById("date").innerText = new Date().getFullYear();
