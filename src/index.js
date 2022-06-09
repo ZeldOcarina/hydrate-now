e608b23fadacdf2b1f3b924772683e4fd86f07f3
ZeldOcarina <42982095+ZeldOcarina@users.noreply.github.com>
2022-06-09 15:07:08 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+if (module.hot) {
+    module.hot.accept();
+}
+
+console.log('%c Welcome to Parcel HTML Starter!!', 'background: #bdc7ff; color: darkred; font-size: 24px; font-weight: bold; padding: 10px;');
